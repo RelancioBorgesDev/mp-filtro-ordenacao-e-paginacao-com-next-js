03e14006ee605362189cb5c24f0fc87463cbdd7d
RelancioBorgesDev <relancio.dev@gmail.com>
2024-05-10 15:08:52 -0300

    refactor: ajuste na funcao de formatar data

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,7 +1,7 @@
 export function formatDate(date: Date) {
-  const dateObj = new Date(date);
-  const dia = String(dateObj.getDate()).padStart(2, "0");
-  const mes = String(dateObj.getMonth() + 1).padStart(2, "0"); // January é 0!
-  const ano = dateObj.getFullYear();
-  return `${dia}/${mes}/${ano}`;
+  const newDate = new Date(date);
+  const day = newDate.getDay();
+  const month = newDate.getMonth();
+  const year = newDate.getFullYear();
+  return `${day}/${month}/${year}`;
 }
